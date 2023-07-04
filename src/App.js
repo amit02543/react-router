734fc1892954bec94c2e4f37be69a2b69b60b01b
Amit Kumar <amit02543@yahoo.com>
2023-07-04 12:38:08 +0530

    added navigation pages

--- src/App.js
+++ src/App.js
@@ -2,23 +2,33 @@ import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } 
 
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
 
 
-const routeDefinitions = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/products" element={<ProductsPage />} />
-  </Route>
-);
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>
+// );
 
 
-const router = createBrowserRouter(routeDefinitions);
+// const router = createBrowserRouter(routeDefinitions);
 
 
-// const router = createBrowserRouter([
-//   { path: '/', element: <HomePage /> },
-//   { path: '/products', element: <ProductsPage /> }
-// ]);
+const router = createBrowserRouter([
+  { 
+    path: '/', 
+    element: <RootLayout />, 
+    errorElement: <ErrorPage />,
+    children: [
+      { path: '/', element: <HomePage /> },
+      { path: '/products', element: <ProductsPage /> }
+    ]
+  },
+  
+]);
 
 
 
